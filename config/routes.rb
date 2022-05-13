f4424db853359f1c073efd90e5aac27141e59dc9
Yann Bouhadana <yannbouhadana@gmail.com>
2022-05-13 11:01:36 -0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
+  resources :lists do
+    resources :bookmarks
+  end
+
+  resources :movies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
